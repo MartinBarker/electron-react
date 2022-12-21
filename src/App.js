bff87584e30ea65d9ca806dac66cac22b4281352
martinbarker99 <martinbarker99@gmail.com>
2022-12-20 18:55:09 -0800

    'minimal-files'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 const staticElement = <h1>temp element text</h1>;
 
@@ -21,7 +20,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
